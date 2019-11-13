4362178cfcd62d4a0f74e9572a7edeb9331c8ebd
Zsofia Koma <komazsofi@gmail.com>
2019-11-13 11:14:25 +0100

    add horizontal metrics calc

---
+++ src/lidar_process/calc_horizontal.R
@@ -0,0 +1,44 @@
+library(raster)
+library(snow)
+library(rgdal)
+library(stringr)
+
+##
+
+workingdir="D:/Sync/_Amsterdam/03_Paper2_bird_lidar_sdm/Analysis2019Nov/"
+setwd(workingdir)
+
+filelist=list.files(pattern = "*95_normalized_height.tif")
+
+for (i in filelist) {
+  print(i)
+  
+  dsm=raster(i)
+  proj4string(dsm) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+  
+  rough_dsm=terrain(dsm,opt="roughness",neighbors=4)
+  
+  height_class=reclassify(dsm, c(-Inf,4,1, 4,Inf,0))
+  
+  lowveg=focal(height_class,w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE)
+  
+  beginCluster(3)
+  
+  sd_dsm_5=clusterR(dsm, focal, args=list(w=matrix(1,5,5), fun=sd, pad=TRUE,na.rm = TRUE))
+  med_dsm_5=clusterR(dsm, focal, args=list(w=matrix(1,5,5), fun=median, pad=TRUE,na.rm = TRUE))
+  
+  sd_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
+  med_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,11,11), fun=median, pad=TRUE,na.rm = TRUE))
+  
+  endCluster()
+  
+  name=str_sub(i,1,25)
+  
+  writeRaster(rough_dsm,paste(name,"_dsm_roughness_10m.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_dsm_5,paste(name,"_dsm_sd_20m.tif",sep=""),overwrite=TRUE)
+  writeRaster(med_dsm_5,paste(name,"_dsm_med_20m.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_dsm_11,paste(name,"_dsm_sd_50m.tif",sep=""),overwrite=TRUE)
+  writeRaster(med_dsm_11,paste(name,"_dsm_med_50m.tif",sep=""),overwrite=TRUE)
+  writeRaster(lowveg,paste(name,"_lowveg_count_50m.tif",sep=""),overwrite=TRUE)
+  
+}
