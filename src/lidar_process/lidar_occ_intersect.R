d9868441b1654ba2151200a1406d17ff508b74e6
Zsofia Koma <komazsofi@gmail.com>
2020-02-10 13:38:51 +0100

    intersection

--- src/lidar_process/lidar_occ_intersect.R
+++ src/lidar_process/lidar_occ_intersect.R
@@ -8,14 +8,10 @@ library(stringr)
 workingdir="D:/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Trial/masked/"
 setwd(workingdir)
 
-junk <- list.files(pattern="_gps_time.tif")
-file.remove(junk)
+# import bird observation data - need to be set all trough the script the right input shp and the shp should be located in the working directory together with the grd file
 
-junk <- list.files(pattern="_intensity.tif")
-file.remove(junk)
-
-#shp=readOGR(".","birds_swet_presatl")
-shp=readOGR(".","birds_swet_absatl")
+shp=readOGR(".","birds_swet_presatl")
+#shp=readOGR(".","birds_swet_absatl")
 #shp=readOGR(".","GreedW_avi_wacq")
 #shp=readOGR(".","ReedW_avi_wacq")
 #shp=readOGR(".","SaviW_avi_wacq")
@@ -29,37 +25,7 @@ shp_sel=subset(shp.df, select=c("coords.x1","coords.x2","species","occrrnc","id"
 coordinates(shp_sel)=~coords.x1+coords.x2
 proj4string(shp_sel)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-filelist=list.files(pattern = "*.tif") # need to rename the files: same start all-> veg, ahn3cj intead of ud, two numer for tile identification
-
-id=str_sub(filelist,1,25)
-id=unique(id)
-id=str_remove(id, "\\_$")
-
-list_forfea=list.files(pattern=id[1])
-feaname=str_remove(list_forfea, id[1])
-feaname=str_remove(feaname, ".tif")
-
-feaname=unique(feaname)
-
-for (i in id) {
-  print(i)
-  
-  rastlist=list.files(pattern=paste(i,"_",sep=""))
-  feaname=str_remove(rastlist, i)
-  feaname=str_remove(feaname, ".tif")
-  
-  rasters=stack(rastlist)
-  
-  crs(rasters) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
-  names(rasters) <- feaname
-  
-  rasters_50m=aggregate(rasters,fact=5,fun=median)
-  
-  writeRaster(rasters_50m,paste(i,"merged_50m.grd",sep=""),overwrite=TRUE)
-  
-}
-
-grdlist=list.files(pattern = "*.grd")
+grdlist=list.files(pattern = "*_onlywetland.grd")
 
 for (j in grdlist) {
   print(j)
@@ -80,8 +46,8 @@ for (j in grdlist) {
     
   name=sub('\\..*', "", j)
   
-  #write.csv(intersect_data,paste(name,"_intersected.csv",sep=""))
-  write.csv(intersect_data,paste(name,"_intersected_abs.csv",sep=""))
+  write.csv(intersect_data,paste(name,"_intersected.csv",sep=""))
+  #write.csv(intersect_data,paste(name,"_intersected_abs.csv",sep=""))
   #write.csv(intersect_data,paste(name,"_intersected_GreedW.csv",sep=""))
   #write.csv(intersect_data,paste(name,"_intersected_ReedW.csv",sep=""))
   #write.csv(intersect_data,paste(name,"_intersected_SaviW.csv",sep=""))
@@ -89,8 +55,8 @@ for (j in grdlist) {
 
 }
 
-#files <- list.files(pattern = "*_intersected.csv")
-files <- list.files(pattern = "*_intersected_abs.csv")
+files <- list.files(pattern = "*_intersected.csv")
+#files <- list.files(pattern = "*_intersected_abs.csv")
 #files <- list.files(pattern = "*_intersected_GreedW.csv")
 #files <- list.files(pattern = "*_intersected_ReedW.csv")
 #files <- list.files(pattern = "*_intersected_SaviW.csv")
@@ -102,8 +68,8 @@ allcsv <- lapply(files,function(g){
 
 allcsv_df <- do.call(rbind.data.frame, allcsv)
 
-#write.csv(allcsv_df,"veg_metrics_50m.csv")
-write.csv(allcsv_df,"veg_metrics_10m_abs.csv")
+write.csv(allcsv_df,"veg_metrics_50m_pres.csv")
+#write.csv(allcsv_df,"veg_metrics_10m_abs.csv")
 #write.csv(allcsv_df,"veg_metrics_10m_GreedW.csv")
 #write.csv(allcsv_df,"veg_metrics_10m_ReedW.csv")
 #write.csv(allcsv_df,"veg_metrics_10m_SaviW.csv")
