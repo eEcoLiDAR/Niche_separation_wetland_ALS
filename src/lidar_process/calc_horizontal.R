f63766570d88dc1d16ae7c5882fec27354639b60
komazsofi <komazsofi@gmail.com>
2020-02-10 18:14:05 +0100

    change radii for hor cal

--- src/lidar_process/calc_horizontal.R
+++ src/lidar_process/calc_horizontal.R
@@ -16,6 +16,7 @@ workingdir="D:/Koma/_PhD/Chapter3/Data_Preprocess/escience_lidar_data/masked/"
 setwd(workingdir)
 
 filelist=list.files(pattern = "*95_normalized_height_masked.tif")
+radii=21
 
 for (i in filelist) {
   print(i)
@@ -26,10 +27,10 @@ for (i in filelist) {
   #create height classes
   
   height_class=reclassify(dsm, c(-Inf,5,1, 5,Inf,0))
-  prop_lowveg=focal(height_class,w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE)
+  prop_lowveg=focal(height_class,w=matrix(1,radii,radii), fun=sum, pad=TRUE,na.rm = TRUE)
   
   height_class2=reclassify(dsm, c(-Inf,10,0, 10,Inf,1))
-  prop_trees=focal(height_class2,w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE)
+  prop_trees=focal(height_class2,w=matrix(1,radii,radii), fun=sum, pad=TRUE,na.rm = TRUE)
   
   #calc. hor variability
   
@@ -38,8 +39,8 @@ for (i in filelist) {
   
   beginCluster(2)
   
-  sd_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
-  sd_lowveg_11=clusterR(lowveg, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
+  sd_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
+  sd_lowveg_11=clusterR(lowveg, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
   
   endCluster()
   
@@ -47,10 +48,10 @@ for (i in filelist) {
   
   name=str_sub(i,1,-5)
   
-  writeRaster(sd_dsm_11,paste(name,"_dsm_sd_50m_masked.tif",sep=""),overwrite=TRUE)
-  writeRaster(sd_lowveg_11,paste(name,"_lowveg_sd_50m_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_dsm_11,paste(name,"_dsm_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_lowveg_11,paste(name,"_lowveg_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
   
-  writeRaster(prop_lowveg,paste(name,"_prop_lowveg_50m_masked.tif",sep=""),overwrite=TRUE)
-  writeRaster(prop_trees,paste(name,"_prop_trees_50m_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(prop_lowveg,paste(name,"_prop_lowveg_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(prop_trees,paste(name,"_prop_trees_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
   
 }
