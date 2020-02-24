d2c1710a851e2b34f6ef87035c10270860a80a5a
komazsofi <komazsofi@gmail.com>
2020-02-24 19:27:34 +0100

    mask and export

--- src/lidar_process/lidarmetrics_permetric_intersect.R
+++ src/lidar_process/lidarmetrics_permetric_intersect.R
@@ -15,8 +15,15 @@ filelist=list.files(pattern = "*.tif")
 lidar=stack(filelist)
 
 lidar_sel <- subset(lidar, c(1,2,3,4,6,9,12,15,19,20), drop=FALSE)
-plot(lidar_sel)
 proj4string(lidar_sel) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
+# import wetland mask
 
+wetland_mask=stack("D:/Koma/_PhD/Chapter3/Data_Preprocess/input_formask/merged_mask_onlywetland.tif")
+proj4string(wetland_mask) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
+# mask
+
+lidar_masked <- mask(lidar_sel, wetland_mask)
+writeRaster(lidar_masked,filename=paste(names(lidar_masked),"_onlywetland",sep=""), bylayer=TRUE,format="GTiff",overwrite=TRUE)
+plot(lidar_masked)
