8dfd299a786b7a4e5af7947ea3e2d21d58e1b07e
komazsofi <komazsofi@gmail.com>
2020-02-24 10:21:41 +0100

    create mask

---
+++ src/lidar_process/CreateMask_onlywetland.R
@@ -0,0 +1,52 @@
+"
+@author: Zsofia Koma, UvA
+Aim: Create wetland mask
+"
+library(gdalUtils)
+library(rgdal)
+library(raster)
+library(dplyr)
+library(stringr)
+
+# Initialize
+
+workingdirectory="D:/Koma/_PhD/Chapter3/Data_Preprocess/escience_lidar_data_v2/selected_layers_for_chapter3/masked/"
+setwd(workingdirectory)
+
+dir.create("onlywetland")
+
+landcoverfile="D:/Koma/_PhD/Chapter3/Data_Preprocess/input_formask/LGN2018.tif"
+
+#Import
+
+landcover=stack(landcoverfile)
+proj4string(landcover) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# loop over all the tif files one by one
+
+filelist=list.files(pattern = "*95_normalized_height_masked.tif")
+
+for (i in filelist) {
+  print(i)
+  
+  getfilename=str_sub(i,1,-5)
+  
+  lidar=stack(i)
+  proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+  
+  landcover_sel=crop(landcover,extent(lidar))
+  
+  lgn8_water_mask <- setValues(raster(landcover_sel), NA)
+  lgn8_water_mask[landcover_sel==16 | landcover_sel==17 | landcover_sel==30 | landcover_sel==322 | landcover_sel==323 | landcover_sel==332 | landcover_sel==333 | landcover_sel==41 | landcover_sel==42 | landcover_sel==43 
+                  | landcover_sel==45 | landcover_sel==46 | landcover_sel==47] <- 1
+  
+  lgn8_water_mask_resampled=resample(lgn8_water_mask,lidar)
+  
+  writeRaster(lgn8_water_mask_resampled,paste(workingdirectory,"/onlywetland/",getfilename,"_onlywetland_mask.tif",sep=""),overwrite=TRUE)
+  
+  # apply
+  lidar_masked <- mask(lidar, lgn8_water_mask_resampled)
+  
+  writeRaster(lidar_masked,paste(workingdirectory,"/onlywetland/",getfilename,"_onlywetland.tif",sep=""),overwrite=TRUE)
+ 
+}
