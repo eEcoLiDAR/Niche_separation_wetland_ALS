b41a4d8797baca46cf8ffef00baf0d0672a75d9d
komazsofi <komazsofi@gmail.com>
2020-03-06 14:04:23 +0100

    add sp per sp intersections

--- src/lidar_process/lidarmetrics_intersect_permetric.R
+++ src/lidar_process/lidarmetrics_intersect_permetric.R
@@ -16,7 +16,7 @@ lidarlist=list.files(pattern = "*_onlywetland.tif")
 lidar=stack(lidarlist)
 proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
-# import bird and prepare data for intersection
+## import GrW and prepare data for intersection
 
 GrW_abs=readOGR(".","GrW_genabs")
 GrW_pres=readOGR(".","GrW_territory_wextra")
@@ -36,7 +36,69 @@ GrW_ter=rbind(GrW_pres_sel,GrW_abs_sel)
 coordinates(GrW_ter)=~coords.x1+coords.x2
 proj4string(GrW_ter)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
+# intersect
 d <- sdmData(occurrence~.,train=GrW_ter,predictors = lidar)
 data=d@features
 
-write.csv(data,"birds_swet_presatl_intersected.csv")
+data$occurrence <- 0
+data$occurrence[1:length(d@species[["occurrence"]]@presence)] <- 1
+
+write.csv(data,"GrW_territory_intersected.csv")
+
+## import Sn and prepare data for intersection
+
+Sn_abs=readOGR(".","Sn_genabs")
+Sn_pres=readOGR(".","Sn_territory_wextra")
+
+Sn_pres.df <- as(Sn_pres, "data.frame")
+Sn_pres.df$id<- seq(1,length(Sn_pres.df$occrrn))
+
+Sn_pres_sel=subset(Sn_pres.df, select=c("coords.x1","coords.x2","species","occrrnc","id"))
+names(Sn_pres_sel)<- c("coords.x1","coords.x2","species","occurrence","id")
+
+Sn_abs.df <- as(Sn_abs, "data.frame")
+Sn_abs_sel=subset(Sn_abs.df, select=c("coords.x1","coords.x2","species","occurrence"))
+Sn_abs_sel$id <- 0
+
+Sn_ter=rbind(Sn_pres_sel,Sn_abs_sel)
+
+coordinates(Sn_ter)=~coords.x1+coords.x2
+proj4string(Sn_ter)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# intersect
+d <- sdmData(occurrence~.,train=Sn_ter,predictors = lidar)
+data=d@features
+
+data$occurrence <- 0
+data$occurrence[1:length(d@species[["occurrence"]]@presence)] <- 1
+
+write.csv(data,"Sn_territory_intersected.csv")
+
+## import KK and prepare data for intersection
+
+KK_abs=readOGR(".","KK_genabs")
+KK_pres=readOGR(".","KK_territory_wextra")
+
+KK_pres.df <- as(KK_pres, "data.frame")
+KK_pres.df$id<- seq(1,length(KK_pres.df$occrrn))
+
+KK_pres_sel=subset(KK_pres.df, select=c("coords.x1","coords.x2","species","occrrnc","id"))
+names(KK_pres_sel)<- c("coords.x1","coords.x2","species","occurrence","id")
+
+KK_abs.df <- as(KK_abs, "data.frame")
+KK_abs_sel=subset(KK_abs.df, select=c("coords.x1","coords.x2","species","occurrence"))
+KK_abs_sel$id <- 0
+
+KK_ter=rbind(KK_pres_sel,KK_abs_sel)
+
+coordinates(KK_ter)=~coords.x1+coords.x2
+proj4string(KK_ter)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# intersect
+d <- sdmData(occurrence~.,train=KK_ter,predictors = lidar)
+data=d@features
+
+data$occurrence <- 0
+data$occurrence[1:length(d@species[["occurrence"]]@presence)] <- 1
+
+write.csv(data,"KK_territory_intersected.csv")
