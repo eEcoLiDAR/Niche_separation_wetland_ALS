a35f132a6005ed92b8ced6571f4ad2a14dc213b1
komazsofi <komazsofi@gmail.com>
2020-03-09 15:14:13 +0100

    export lower resolution for lanscapemetrics

--- src/lidar_process/lidarmetrics_applymask.R
+++ src/lidar_process/lidarmetrics_applymask.R
@@ -14,7 +14,8 @@ filelist=list.files(pattern = "*.tif")
 
 lidar=stack(filelist)
 
-lidar_sel <- subset(lidar, c(1,2,3,4,6,9,12,15,19,20), drop=FALSE)
+#lidar_sel <- subset(lidar, c(1,2,3,4,6,9,12,15,19,20), drop=FALSE)
+lidar_sel <- subset(lidar, c(7,8,10,11,13,14), drop=FALSE)
 proj4string(lidar_sel) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 # import wetland mask (enough to do once)
