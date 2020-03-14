371f019fe881c9a6e450d7a3a9aa4d5f8d435b58
komazsofi <komazsofi@gmail.com>
2020-03-14 13:10:29 +0100

    tried to add landscapemetrics .. parallelization not working...

--- src/lidar_process/calc_horizontal.R
+++ src/lidar_process/calc_horizontal.R
@@ -12,13 +12,13 @@ library(spatialEco)
 ##
 
 #workingdir="D:/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Trial/masked/"
-workingdir="D:/Koma/_PhD/Chapter3/Data_Preprocess/escience_lidar_data_v2/selected_layers_for_chapter3/masked/"
+workingdir="D:/Koma/_PhD/Offline/Chapter3/Data_Preprocess/escience_lidar_data_v2/selected_layers_for_chapter3/masked2/"
 setwd(workingdir)
 
 filelist=list.files(pattern = "*95_normalized_height_masked.tif")
-radii=51
+radii=21
 
-for (i in filelist) {
+for (i in filelist[1]) {
   print(i)
   
   dsm=raster(i)
@@ -29,19 +29,24 @@ for (i in filelist) {
   height_class=reclassify(dsm, c(-Inf,5,1, 5,Inf,0))
   prop_lowveg=focal(height_class,w=matrix(1,radii,radii), fun=sum, pad=TRUE,na.rm = TRUE)
   
-  height_class2=reclassify(dsm, c(-Inf,10,0, 10,Inf,1))
-  prop_trees=focal(height_class2,w=matrix(1,radii,radii), fun=sum, pad=TRUE,na.rm = TRUE)
+  #dsm_allin=dsm
+  #dsm_allin[is.na(dsm_allin)] <- 0
+  
+  #height_class2=reclassify(dsm_allin, c(-Inf,0,1, 0,5,2, 5,Inf,3))
+  #proj4string(height_class2) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
   
   #calc. hor variability
   
   lowveg=dsm
   lowveg[lowveg>5] <- NA
   
-  beginCluster(2)
+  beginCluster(15)
   
   sd_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
   sd_lowveg_11=clusterR(lowveg, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
-  
+  #landsc_m_mv_np=clusterR(height_class2,focal.lmetrics,args=list(w=radii, metric = "n.patches"))
+  #landsc_m_mv_ed=clusterR(height_class2,focal.lmetrics,args=list(w=radii, land.value = 2, metric = "edge.density"))
+
   endCluster()
   
   # export
@@ -52,6 +57,8 @@ for (i in filelist) {
   writeRaster(sd_lowveg_11,paste(name,"_lowveg_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
   
   writeRaster(prop_lowveg,paste(name,"_prop_lowveg_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
-  writeRaster(prop_trees,paste(name,"_prop_trees_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  
+  #writeRaster(landsc_m_mv_np,paste(name,"_landscape_npatch_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  #writeRaster(landsc_m_mv_ed,paste(name,"_landscape_edgeden_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
   
 }
--- src/lidar_process/lidarmetrics_merge_permetric.R
+++ src/lidar_process/lidarmetrics_merge_permetric.R
@@ -37,7 +37,7 @@ mosaicList <- function(rasList){
 
 ####
 
-workingdir="D:/Koma/_PhD/Chapter3/Data_Preprocess/escience_lidar_data_v2/selected_layers_for_chapter3/masked/"
+workingdir="D:/Koma/_PhD/Offline/Chapter3/Data_Preprocess/escience_lidar_data_v2/selected_layers_for_chapter3/masked2/"
 setwd(workingdir)
 
 filelist=list.files(pattern = "*.tif") # need to rename the files: same start all-> veg, ahn3cj intead of ud, two numer for tile identification
@@ -45,7 +45,7 @@ filelist=list.files(pattern = "*.tif") # need to rename the files: same start al
 
 # import landcover
 
-landcoverfile="D:/Koma/_PhD/Chapter3/Data_Preprocess/input_formask/LGN2018.tif"
+landcoverfile="D:/Koma/_PhD/Offline/Chapter3/Data_Preprocess/input_formask/LGN2018.tif"
 landcover=stack(landcoverfile)
 proj4string(landcover) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
