266d7d5190b8dd14451478bb88eb640f0306442a
komazsofi <komazsofi@gmail.com>
2020-03-31 15:43:36 +0200

    raster and cross plot

--- src/analysis/Fig2_pcloudvis.R
+++ src/analysis/Fig2_pcloudvis.R
@@ -35,53 +35,89 @@ plot(clipped_6, color = "Classification")
 
 # Visualization with Plot3D
 
+clipped_1=readLAS("grw_1.laz")
+clipped_2=readLAS("sn_1.laz")
+clipped_3=readLAS("kk_1.laz")
+
 clipped_4=readLAS("grw_2.laz")
 clipped_5=readLAS("sn_2.laz")
 clipped_6=readLAS("kk_2.laz")
 
-# normalize point cloud
+#rasterplot
 
-clipped_4_nonveg = lasfilter(clipped_4, Classification != 1)
+rasterplot<-function(clipped_4,x=184877,y=511157,bird="Great reed warbler"){
+  
+  clipped_4_nonveg = lasfilter(clipped_4, Classification != 1)
+  
+  dtm = grid_terrain(clipped_4_nonveg, res = 1, algorithm = knnidw(k = 25L))
+  lasnormalize(clipped_4, dtm)
+  
+  hperc09 = grid_metrics(clipped_4, quantile(Z, 0.90), res=1)
+  plot(hperc09)
+  crs(hperc09) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
+  height_class=reclassify(hperc09, c(-Inf,1,1,1,3,2,3,5,3,5,Inf,4))
+  
+  slope <- terrain(hperc09, opt='slope')
+  aspect <- terrain(hperc09, opt='aspect')
+  dsm_shd <- hillShade(slope, aspect, 40, 270)
+  
+  coords = matrix(c(x, y), 
+                  ncol = 2, byrow = TRUE)
+  
+  
+  birdpoint = SpatialPoints(coords, proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
+  
+  coords = matrix(c(x, y-10,
+                    x, y+10), 
+                  ncol = 2, byrow = TRUE)
+  
+  
+  P1 = Polygon(coords)
+  line_cr = SpatialPolygons(list(Polygons(list(P1), ID = "a")), proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
+  
+  coords2 = matrix(c(x-10, y,
+                     x+10, y), 
+                   ncol = 2, byrow = TRUE)
+  
+  
+  P12 = Polygon(coords2)
+  line_cr2 = SpatialPolygons(list(Polygons(list(P12), ID = "a")), proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
+  
+  par(mfrow=c(1,1)) 
+  plot(dsm_shd, col=grey(0:100/100), legend=FALSE, main=bird)
+  plot(height_class, col=rainbow(4, alpha=0.35),breaks=c(0,1,2,3,4), add=TRUE,
+       lab.breaks = c("0","1","3","5","20"),
+       legend.args=list(text='Height [m]', side=4, font=2, line=2.5, cex=1.5))
+  plot(birdpoint,pch=1,cex=3,lwd = 3,add=TRUE)
+  plot(line_cr, lwd=3,add=TRUE)
+  plot(line_cr2, lwd=3,add=TRUE)
+  
+}
 
-dtm = grid_terrain(clipped_4_nonveg, res = 1, algorithm = knnidw(k = 25L))
-lasnormalize(clipped_4, dtm)
+rasterplot(clipped_4,x=184877,y=511157,bird="Great reed warbler")
+rasterplot(clipped_5,x=184606,y=510973,bird="Savi's warbler")
+rasterplot(clipped_3,x=198263,y=516790,bird="Reed warbler")
 
-hperc09 = grid_metrics(clipped_4, quantile(Z, 0.90), res=1)
-plot(hperc09)
-crs(hperc09) <- "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"
+#crossplot
 
-slope <- terrain(hperc09, opt='slope')
-aspect <- terrain(hperc09, opt='aspect')
-dsm_shd <- hillShade(slope, aspect, 40, 270)
+crossplot<-function(clipped_4,x=184877,y=511157){
+  las_cross_ver=lasclipRectangle(clipped_4,x-1,y-60,x+1,y+40)
+  las_cross_ver@data$cross=(las_cross_ver@data$Y-y+100)-40
+  
+  plot(x = las_cross_ver@data$cross, 
+       y = las_cross_ver@data$Z, col = c("green", "orange", "blue","blue","blue","red","blue","blue","blue","blue")[las_cross_ver@data$Classification],
+       frame = FALSE, 
+       xlab = "Distance[m]", ylab = "Height[m]",pch=19,ylim=c(0,20),
+       main="Observation point")
+  abline(v=60,lty=3)
+  legend("topright",legend=c("Ground","Vegetation","Water"),xpd=TRUE,pch=19,col = c("orange", "green","blue"))
+  
+}
 
-coords = matrix(c(184877, 511157), 
-                ncol = 2, byrow = TRUE)
+crossplot(clipped_4,x=184877,y=511157)
+crossplot(clipped_5,x=184606,y=510973)
+crossplot(clipped_3,x=198263,y=516790)
 
+# 3D pcloudpot
 
-birdpoint = SpatialPoints(coords, proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
-
-coords = matrix(c(184877, 511157-10,
-                  184877, 511157+10), 
-                ncol = 2, byrow = TRUE)
-
-
-P1 = Polygon(coords)
-line_cr = SpatialPolygons(list(Polygons(list(P1), ID = "a")), proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
-
-coords2 = matrix(c(184877-10, 511157,
-                   184877+10, 511157), 
-                 ncol = 2, byrow = TRUE)
-
-
-P12 = Polygon(coords2)
-line_cr2 = SpatialPolygons(list(Polygons(list(P12), ID = "a")), proj4string=CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs"))
-
-
-par(mfrow=c(1,1)) 
-plot(dsm_shd, col=grey(0:100/100), legend=FALSE, main="Great reed warbler")
-plot(hperc09, col=rainbow(25, alpha=0.35), add=TRUE,legend.args=list(text='Height [m]', side=4, font=2, line=2.5, cex=1.5))
-plot(birdpoint,pch=1,cex=3,lwd = 3,add=TRUE)
-plot(line_cr, lwd=3,add=TRUE)
-plot(line_cr2, lwd=3,add=TRUE)
-
-# 2D raster
+plot(clipped_4)
