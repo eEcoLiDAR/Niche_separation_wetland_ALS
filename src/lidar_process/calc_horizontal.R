69c6722e3e45a0a062637f539083a1b7c553f9b7
komazsofi <komazsofi@gmail.com>
2020-04-09 10:00:42 +0200

    calc sd for reed and bushes

--- src/lidar_process/calc_horizontal.R
+++ src/lidar_process/calc_horizontal.R
@@ -8,6 +8,36 @@ library(dplyr)
 
 library(spatialEco)
 
+#### function
+
+mosaicList <- function(rasList){
+  
+  #Internal function to make a list of raster objects from list of files.
+  ListRasters <- function(list_names) {
+    raster_list <- list() # initialise the list of rasters
+    for (i in 1:(length(list_names))){ 
+      grd_name <- list_names[i] # list_names contains all the names of the images in .grd format
+      raster_file <- raster::raster(grd_name)
+    }
+    raster_list <- append(raster_list, raster_file) # update raster_list at each iteration
+  }
+  
+  #convert every raster path to a raster object and create list of the results
+  raster.list <-sapply(rasList, FUN = ListRasters)
+  
+  # edit settings of the raster list for use in do.call and mosaic
+  names(raster.list) <- NULL
+  #####This function deals with overlapping areas
+  raster.list$fun <- mean
+  
+  #run do call to implement mosaic over the list of raster objects.
+  mos <- do.call(raster::mosaic, raster.list)
+  
+  #set crs of output
+  crs(mos) <- crs(x = raster(rasList[1]))
+  return(mos)
+}
+
 
 ##
 
@@ -24,28 +54,17 @@ for (i in filelist) {
   dsm=raster(i)
   proj4string(dsm) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
   
-  #create height classes
+  reedveg=dsm
+  reedveg[(reedveg<1 | reedveg>3)] <- NA
   
-  height_class=reclassify(dsm, c(-Inf,5,1, 5,Inf,0))
-  prop_lowveg=focal(height_class,w=matrix(1,radii,radii), fun=sum, pad=TRUE,na.rm = TRUE)
-  
-  #dsm_allin=dsm
-  #dsm_allin[is.na(dsm_allin)] <- 0
-  
-  #height_class2=reclassify(dsm_allin, c(-Inf,0,1, 0,5,2, 5,Inf,3))
-  #proj4string(height_class2) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-  
-  #calc. hor variability
-  
-  lowveg=dsm
-  lowveg[lowveg>5] <- NA
+  bushveg=dsm
+  bushveg[(bushveg<3 | bushveg>5)] <- NA
   
   beginCluster(15)
   
-  sd_dsm_11=clusterR(dsm, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
-  sd_lowveg_11=clusterR(lowveg, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
-  #landsc_m_mv_np=clusterR(height_class2,focal.lmetrics,args=list(w=radii, metric = "n.patches"))
-  #landsc_m_mv_ed=clusterR(height_class2,focal.lmetrics,args=list(w=radii, land.value = 2, metric = "edge.density"))
+  sd_dsm_21=clusterR(dsm, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
+  sd_reedveg_21=clusterR(reedveg, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
+  sd_bushveg_21=clusterR(bushveg, focal, args=list(w=matrix(1,radii,radii), fun=sd, pad=TRUE,na.rm = TRUE))
 
   endCluster()
   
@@ -53,12 +72,21 @@ for (i in filelist) {
   
   name=str_sub(i,1,-5)
   
-  writeRaster(sd_dsm_11,paste(name,"_dsm_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
-  writeRaster(sd_lowveg_11,paste(name,"_lowveg_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_dsm_21,paste(name,"_dsm_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_reedveg_21,paste(name,"_reedveg_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(sd_bushveg_21,paste(name,"_bushveg_sd_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
   
-  writeRaster(prop_lowveg,paste(name,"_prop_lowveg_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+}
+
+feaname=c("_perc_95_normalized_height_masked_dsm_sd_21p_masked","_perc_95_normalized_height_masked_reedveg_sd_21p_masked",
+          "_perc_95_normalized_height_masked_bushveg_sd_21p_masked")
+
+for (i in feaname) {
+  print(i)
   
-  #writeRaster(landsc_m_mv_np,paste(name,"_landscape_npatch_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
-  #writeRaster(landsc_m_mv_ed,paste(name,"_landscape_edgeden_",radii,"p_masked.tif",sep=""),overwrite=TRUE)
+  files_permetric=list.files(pattern = paste("*",i,".tif",sep=""))
+  metric=mosaicList(files_permetric)
+  
+  writeRaster(metric,paste("lidar_metric",i,"_all.tif",sep=""),overwrite=TRUE)
   
 }
