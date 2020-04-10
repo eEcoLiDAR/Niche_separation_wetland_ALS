e4ad8a96db2a2a032bd2162b953980cea12a53b1
komazsofi <komazsofi@gmail.com>
2020-04-10 14:50:13 +0200

    intersecting survey plot based pres abs

--- src/lidar_process/lidarmetrics_intersect_permetric.R
+++ src/lidar_process/lidarmetrics_intersect_permetric.R
@@ -6,38 +6,6 @@ library(stringr)
 
 ## function for intersection
 
-IntersectForAnal = function(GrW_pres,GrW_abs,landcover,lidar,name='GrW') {
-  GrW_pres.df <- as(GrW_pres, "data.frame")
-  GrW_pres.df$x<- GrW_pres.df$coords.x1
-  GrW_pres.df$y<- GrW_pres.df$coords.x2
-  
-  GrW_pres_sel=subset(GrW_pres.df, select=c("coords.x1","coords.x2","species","occrrnc","x","y"))
-  names(GrW_pres_sel)<- c("coords.x1","coords.x2","species","occurrence","x","y")
-  
-  GrW_abs.df <- as(GrW_abs, "data.frame")
-  GrW_abs_sel=subset(GrW_abs.df, select=c("coords.x1","coords.x2","species","occurrence","x","y"))
-  
-  GrW_ter=rbind(GrW_pres_sel,GrW_abs_sel)
-  
-  coordinates(GrW_ter)=~coords.x1+coords.x2
-  proj4string(GrW_ter)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-  
-  # add lgn8 info
-  GrW_ter_lgn8=raster::extract(landcover,GrW_ter)
-  GrW_ter$lgn8=GrW_ter_lgn8[,1]
-  
-  # intersect
-  d <- sdmData(occurrence~.,train=GrW_ter,predictors = lidar)
-  write.sdm(d,name,overwrite=TRUE) 
-  data=d@features
-  
-  data$occurrence <- 0
-  data$occurrence[1:length(d@species[["occurrence"]]@presence)] <- 1
-  
-  return(data)
-  
-}
-
 IntersectForAnal_single = function(GrW_pres,landcover,lidar,name='GrW') {
   GrW_pres.df <- as(GrW_pres, "data.frame")
   GrW_pres.df$x<- GrW_pres.df$coords.x1
@@ -67,7 +35,7 @@ IntersectForAnal_single = function(GrW_pres,landcover,lidar,name='GrW') {
 
 rasterOptions(maxmemory = 100000000000)
 
-workingdir="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v5/"
+workingdir="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v6/"
 setwd(workingdir)
 
 # Work with the masked files
@@ -83,8 +51,7 @@ proj4string(landcover) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38
 
 ## GrW
 
-#GrW_abs=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/GrW_genabs_20.shp")
-GrW_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/GrW_territory_20.shp")
+GrW_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v5/GrW_territory.shp")
 
 data_GrW=IntersectForAnal_single(GrW_pres,landcover,lidar,name='GrW')
 
@@ -92,8 +59,7 @@ write.csv(data_GrW,"GrW_territory_intersected.csv")
 
 ## Sn
 
-#Sn_abs=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/Sn_genabs_20.shp")
-Sn_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/Sn_territory_20.shp")
+Sn_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v5/Sn_territory.shp")
 
 data_Sn=IntersectForAnal_single(Sn_pres,landcover,lidar,name='Sn')
 
@@ -101,29 +67,16 @@ write.csv(data_Sn,"Sn_territory_intersected.csv")
 
 ## KK
 
-#KK_abs=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/KK_genabs_20.shp")
-KK_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v3/KK_territory_20.shp")
+KK_pres=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v5/KK_territory.shp")
 
 data_KK=IntersectForAnal_single(KK_pres,landcover,lidar,name='KK')
 
 write.csv(data_KK,"KK_territory_intersected.csv")
 
 ## Background
-Bgr=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v4/Bgr_20.shp")
+Bgr=readOGR(dsn="D:/Koma/_PhD/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Process_birddata_v5/Bgr.shp")
 names(Bgr)<- c("coords.x1","coords.x2","species","occrrnc")
 
 data_Bgr=IntersectForAnal_single(Bgr,landcover,lidar,name='Background')
 
 write.csv(data_Bgr,"Bgr_territory_intersected.csv")
-
-## check number of pres-abs
-
-GrW_lgn8 <- subset(data_GrW, lgn8 %in% c(16,17,30,322,332,41,42))
-KK_lgn8 <- subset(data_KK, lgn8 %in% c(16,17,30,322,332,41,42))
-Sn_lgn8 <- subset(data_Sn, lgn8 %in% c(16,17,30,322,332,41,42))
-
-data_merged=rbind(GrW_lgn8,KK_lgn8,Sn_lgn8)
-
-data_presabs_stat <- data_merged %>%
-  group_by(species,occurrence) %>%
-  summarise(nofobs = length(occurrence))
