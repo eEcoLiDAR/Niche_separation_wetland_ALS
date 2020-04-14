047d30be1fd2647e4f98f63683e64a57c8508d97
komazsofi <komazsofi@gmail.com>
2020-04-14 16:58:31 +0200

    calc convex hull area

--- src/analysis/Fig_pca.R
+++ src/analysis/Fig_pca.R
@@ -34,7 +34,7 @@ Bgr_lgn8 <- subset(Bgr, lgn8 %in% c(16,17,30,322,332,41,42,43))
 #data_merged=rbind(GrW_lgn8,KK_lgn8,Sn_lgn8,Bgr_lgn8)
 data_merged=rbind(GrW,KK,Sn,Bgr)
 
-noffea=12
+noffea=10
 
 # 100 m with bushveg
 data_merged=subset(data_merged,select=c(11,10,9,7,8,14,20,18,25,29,12,16,22,23,6,4,5,3))
@@ -129,6 +129,33 @@ ggsave("Fig2_pca_a_200mreed_wlgn8.png",plot = fig2,width = 15, height = 12)
 
 var <- get_pca_var(pca.env)
 corrplot(as.matrix(var$cor), is.corr=FALSE,method="number",col=colorRampPalette(c("dodgerblue4","white","firebrick"))(200))
+
+# specialization (convex hull area)
+grotekarakiet=dplyr::filter(data_merged,str_detect(data_merged$species,"Grote Karekiet"))
+kleinekarakiet=dplyr::filter(data_merged,str_detect(data_merged$species,"Kleine Karekiet"))
+snor=dplyr::filter(data_merged,str_detect(data_merged$species,"Snor"))
+bgr=dplyr::filter(data_merged,str_detect(data_merged$species,"Background"))
+
+scores.sp.grotekarakiet<-suprow(pca.env,grotekarakiet[,1:noffea])$li
+scores.sp.kleinekarakiet<-suprow(pca.env,kleinekarakiet[,1:noffea])$li
+scores.sp.snor<-suprow(pca.env,snor[,1:noffea])$li
+scores.clim.background<-suprow(pca.env,bgr[,1:noffea])$li
+
+convhull_GrW=chull(scores.sp.grotekarakiet[,c(1,2)])
+poly_grw=scores.sp.grotekarakiet[c(convhull_GrW,convhull_GrW[1]),c(1,2)]
+poly_grw_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_grw)), ID=1)))
+
+convhull_kk=chull(scores.sp.kleinekarakiet[,c(1,2)])
+poly_kk=scores.sp.kleinekarakiet[c(convhull_kk,convhull_kk[1]),c(1,2)]
+poly_kk_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_kk)), ID=1)))
+
+convhull_sn=chull(scores.sp.snor[,c(1,2)])
+poly_sn=scores.sp.snor[c(convhull_sn,convhull_sn[1]),c(1,2)]
+poly_sn_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_sn)), ID=1)))
+
+convhull_bgr=chull(scores.clim.background[,c(1,2)])
+poly_bgr=scores.clim.background[c(convhull_bgr,convhull_bgr[1]),c(1,2)]
+poly_bgr_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_bgr)), ID=1)))
 
 
 
