7abda7e41437d1862289ba96befdc038c2a6e0fe
komazsofi <komazsofi@gmail.com>
2020-04-16 17:09:38 +0200

    update pca

--- src/analysis/Fig_pca.R
+++ src/analysis/Fig_pca.R
@@ -13,52 +13,31 @@ library(gridExtra)
 library(GGally)
 
 library(egg)
+library(ecospat)
 
 # Global
-workingdirectory="C:/Koma/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v7/"
+workingdirectory="C:/Koma/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v8/"
 setwd(workingdirectory)
 
 # Import data
 GrW=read.csv("GrW_wlandsc.csv")
-GrW_lgn8 <- subset(GrW, lgn8 %in% c(16,17,30,322,332,41,42,43))
-
 KK=read.csv("KK_wlandsc.csv")
-KK_lgn8 <- subset(KK, lgn8 %in% c(16,17,30,322,332,41,42,43))
-
 Sn=read.csv("Sn_wlandsc.csv")
+Bgr=read.csv("Bgr_wlandsc.csv")
+
+GrW_lgn8 <- subset(GrW, lgn8 %in% c(16,17,30,322,332,41,42,43))
+KK_lgn8 <- subset(KK, lgn8 %in% c(16,17,30,322,332,41,42,43))
 Sn_lgn8 <- subset(Sn, lgn8 %in% c(16,17,30,322,332,41,42,43))
 
-Bgr=read.csv("Bgr_wlandsc.csv")
-Bgr_lgn8 <- subset(Bgr, lgn8 %in% c(16,17,30,322,332,41,42,43))
-
-#data_merged=rbind(GrW_lgn8,KK_lgn8,Sn_lgn8,Bgr_lgn8)
 data_merged=rbind(GrW,KK,Sn,Bgr)
 
 noffea=10
 
-# 100 m with bushveg
-data_merged=subset(data_merged,select=c(11,10,9,7,8,14,20,18,25,29,12,16,22,23,6,4,5,3))
-names(data_merged) <- c("VV_p95","VV_FHD","VD_0_1","VD_1_2","VD_2_3",
-                        "HV_sd","HV_reedveg_sd", "HV_reedveg_prop","HV_reedveg_patch","HV_reedveg_edge",
-                        "HV_bushveg_sd", "HV_bushveg_prop",
-                        "species","occurrence","lgn8","x","y","id")
-# 100 m only reed
-data_merged=subset(data_merged,select=c(11,10,9,7,8,14,20,18,25,29,22,23,6,4,5,3))
-names(data_merged) <- c("VV_p95","VV_FHD","VD_0_1","VD_1_2","VD_2_3",
-                        "HV_sd","HV_reedveg_sd", "HV_reedveg_prop","HV_reedveg_patch","HV_reedveg_edge",
-                        "species","occurrence","lgn8","x","y","id")
-
-# 200 m with bushveg
-data_merged=subset(data_merged,select=c(11,10,9,7,8,15,21,19,33,37,13,17,22,23,6,4,5,3))
-names(data_merged) <- c("VV_p95","VV_FHD","VD_0_1","VD_1_2","VD_2_3",
-                        "HV_sd","HV_reedveg_sd", "HV_reedveg_prop","HV_reedveg_patch","HV_reedveg_edge",
-                        "HV_bushveg_sd", "HV_bushveg_prop",
-                        "species","occurrence","lgn8","x","y","id")
 # 200 m only reed
-data_merged=subset(data_merged,select=c(11,10,9,7,8,15,21,19,33,37,22,23,6,4,5,3))
+data_merged=subset(data_merged,select=c(11,10,9,7,8,12,14,13,18,22,15,16,4,5,2))
 names(data_merged) <- c("VV_p95","VV_FHD","VD_0_1","VD_1_2","VD_2_3",
                         "HV_sd","HV_reedveg_sd", "HV_reedveg_prop","HV_reedveg_patch","HV_reedveg_edge",
-                        "species","occurrence","lgn8","x","y","id")
+                        "species","occurrence","x","y","id")
 
 data_merged=data_merged[(data_merged$VV_p95<30),]
 data_merged[is.na(data_merged)==TRUE] <- 0
@@ -116,9 +95,9 @@ p4=fviz_pca_biplot(pca.env, axes=c(1,2),
 
 fig2=grid.arrange(
   p1+labs(title ="a) PCA variable contribution")+theme(axis.text=element_text(size=14),axis.title=element_text(size=14)),
-  p2+labs(title ="b) Great reed warbler")+xlim(-8,5)+ylim(-5,5)+theme(axis.text=element_text(size=14),axis.title=element_text(size=14)),
-  p3+labs(title ="c) Reed warbler")+xlim(-8,5)+ylim(-5,5)+theme(axis.text=element_text(size=14),axis.title=element_text(size=14)),
-  p4+labs(title ="d) Savi's warbler")+xlim(-8,5)+ylim(-5,5)+theme(axis.text=element_text(size=14),axis.title=element_text(size=14)),
+  p2+labs(title ="b) Great reed warbler")+theme(axis.text=element_text(size=14),axis.title=element_text(size=14))+xlim(-7.5,5)+ylim(-6,5),
+  p3+labs(title ="c) Reed warbler")+theme(axis.text=element_text(size=14),axis.title=element_text(size=14))+xlim(-7.5,5)+ylim(-6,5),
+  p4+labs(title ="d) Savi's warbler")+theme(axis.text=element_text(size=14),axis.title=element_text(size=14))+xlim(-7.5,5)+ylim(-6,5),
   ncol=2,
   nrow=2
 )
@@ -143,19 +122,41 @@ scores.clim.background<-suprow(pca.env,bgr[,1:noffea])$li
 
 convhull_GrW=chull(scores.sp.grotekarakiet[,c(1,2)])
 poly_grw=scores.sp.grotekarakiet[c(convhull_GrW,convhull_GrW[1]),c(1,2)]
-poly_grw_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_grw)), ID=1)))
+poly_grw_1 <- SpatialPolygons(list(Polygons(list(Polygon(poly_grw)), ID=1)))
 
 convhull_kk=chull(scores.sp.kleinekarakiet[,c(1,2)])
 poly_kk=scores.sp.kleinekarakiet[c(convhull_kk,convhull_kk[1]),c(1,2)]
-poly_kk_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_kk)), ID=1)))
+poly_kk_1 <- SpatialPolygons(list(Polygons(list(Polygon(poly_kk)), ID=1)))
 
 convhull_sn=chull(scores.sp.snor[,c(1,2)])
 poly_sn=scores.sp.snor[c(convhull_sn,convhull_sn[1]),c(1,2)]
-poly_sn_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_sn)), ID=1)))
+poly_sn_1 <- SpatialPolygons(list(Polygons(list(Polygon(poly_sn)), ID=1)))
 
 convhull_bgr=chull(scores.clim.background[,c(1,2)])
 poly_bgr=scores.clim.background[c(convhull_bgr,convhull_bgr[1]),c(1,2)]
-poly_bgr_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_bgr)), ID=1)))
+poly_bgr_1 <- SpatialPolygons(list(Polygons(list(Polygon(poly_bgr)), ID=1)))
+
+# 50th percentile
+
+get_opt_chull<-function(scores.sp.grotekarakiet){
+  
+  Grw_p_PCA1=quantile(scores.sp.grotekarakiet[,1],c(.25,.75))
+  Grw_p_PCA2=quantile(scores.sp.grotekarakiet[,2],c(.25,.75))
+  
+  scores.sp.grotekarakiet_sel=scores.sp.grotekarakiet[(scores.sp.grotekarakiet$Axis1>Grw_p_PCA1[1] & scores.sp.grotekarakiet$Axis1<Grw_p_PCA1[2]
+                                                       & scores.sp.grotekarakiet$Axis2>Grw_p_PCA2[1] & scores.sp.grotekarakiet$Axis2<Grw_p_PCA2[2]),]
+  
+  convhull_GrW=chull(scores.sp.grotekarakiet_sel[,c(1,2)])
+  poly_grw=scores.sp.grotekarakiet_sel[c(convhull_GrW,convhull_GrW[1]),c(1,2)]
+  poly_grw_2 <- SpatialPolygons(list(Polygons(list(Polygon(poly_grw)), ID=1)))
+  
+  return(poly_grw_2)
+}
+
+poly_grw_2=get_opt_chull(scores.sp.grotekarakiet)
+poly_kk_2=get_opt_chull(scores.sp.kleinekarakiet)
+poly_sn_2=get_opt_chull(scores.sp.snor)
+poly_bgr_2=get_opt_chull(scores.clim.background)
 
 
 
