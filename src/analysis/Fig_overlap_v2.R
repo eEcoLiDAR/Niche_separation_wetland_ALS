6c93c347dd6217a84d2a1d7efb94953e3437f896
komazsofi <komazsofi@gmail.com>
2020-04-30 15:32:18 +0200

    small changes

--- src/analysis/Fig_overlap_v2.R
+++ src/analysis/Fig_overlap_v2.R
@@ -47,11 +47,11 @@ plot.2niche <- function(kk_pca12,grw_pca12,pal50kk,pal50grw,col1,col2) {
         xlab = "PCA 1", ylab = "PCA 1", add=TRUE)
   
   grw_pca12$uncor.norm<-t(as.matrix(grw_pca12$z.uncor))[,nrow(as.matrix(grw_pca12$z.uncor)):1]
-  contour(x=grw_pca12$x,y=grw_pca12$y,grw_pca12$uncor.norm, levels = quantile(grw_pca12$uncor.norm[grw_pca12$uncor.norm > 0], c(0, 0.5,0.75)), drawlabels = FALSE,
-          lty = c(1, 2,2),col=col1,lwd=2,add=TRUE)
+  contour(x=grw_pca12$x,y=grw_pca12$y,grw_pca12$uncor.norm, levels = quantile(grw_pca12$uncor.norm[grw_pca12$uncor.norm > 0], c(0, 0.5,0.75,0.9)), drawlabels = FALSE,
+          lty = c(1,2,2,2),col=col1,lwd=2,add=TRUE)
   kk_pca12$uncor.norm<-t(as.matrix(kk_pca12$z.uncor))[,nrow(as.matrix(kk_pca12$z.uncor)):1]
-  contour(x=kk_pca12$x,y=kk_pca12$y,kk_pca12$uncor.norm, add = TRUE, levels = quantile(kk_pca12$uncor.norm[kk_pca12$uncor.norm > 0], c(0, 0.5,0.75)), drawlabels = FALSE,
-          lty = c(1, 2,2),col=col2,lwd=2)
+  contour(x=kk_pca12$x,y=kk_pca12$y,kk_pca12$uncor.norm, add = TRUE, levels = quantile(kk_pca12$uncor.norm[kk_pca12$uncor.norm > 0], c(0, 0.5,0.75,0.9)), drawlabels = FALSE,
+          lty = c(1,2,2,2),col=col2,lwd=2)
   kk_pca12$Z<-t(as.matrix(kk_pca12$Z))[,nrow(as.matrix(kk_pca12$Z)):1]
   contour(x=kk_pca12$x,y=kk_pca12$y,kk_pca12$Z, add = TRUE, levels = quantile(kk_pca12$Z[kk_pca12$Z > 0], c(0)), drawlabels = FALSE,
           lty = c(1),lwd=2)
