9ce5da656c56d11205afe6c4bd26a5b978d4a48c
komazsofi <komazsofi@gmail.com>
2020-04-30 20:47:09 +0200

    add other species

--- src/analysis/Fig_overlap_v2.R
+++ src/analysis/Fig_overlap_v2.R
@@ -42,9 +42,9 @@ pal50diff<- paldiff(50)
 # two niche overlap
 plot.2niche <- function(kk_pca12,grw_pca12,pal50kk,pal50grw,col1,col2) {
   image(x=kk_pca12$x,y=kk_pca12$y,z=t(as.matrix(kk_pca12$z.uncor))[,nrow(as.matrix(kk_pca12$z.uncor)):1], col = alpha(pal50kk,1), zlim = c(1e-06, cellStats(kk_pca12$z.cor,"max")), 
-        xlab = "PCA 1", ylab = "PCA 1")
+        xlab = "PCA 1", ylab = "PCA 2")
   image(x=grw_pca12$x,y=grw_pca12$y,z=t(as.matrix(grw_pca12$z.uncor))[,nrow(as.matrix(grw_pca12$z.uncor)):1], col = alpha(pal50grw,0.6), zlim = c(1e-06, cellStats(grw_pca12$z.cor,"max")), 
-        xlab = "PCA 1", ylab = "PCA 1", add=TRUE)
+        xlab = "PCA 1", ylab = "PCA 2", add=TRUE)
   
   grw_pca12$uncor.norm<-t(as.matrix(grw_pca12$z.uncor))[,nrow(as.matrix(grw_pca12$z.uncor)):1]
   contour(x=grw_pca12$x,y=grw_pca12$y,grw_pca12$uncor.norm, levels = quantile(grw_pca12$uncor.norm[grw_pca12$uncor.norm > 0], c(0, 0.5,0.75,0.9)), drawlabels = FALSE,
@@ -59,19 +59,26 @@ plot.2niche <- function(kk_pca12,grw_pca12,pal50kk,pal50grw,col1,col2) {
 }
 
 plot.2niche(kk_pca12,grw_pca12,pal50kk,pal50grw,"goldenrod4","green3")
-title("a) overlap of habitat niches", adj = 0)
+title("a) Great reed warbler compare to reed warbler [D=0.61]", adj = 0)
 legend("topright", legend=c("RW","GrW", "Background"),
        col=c("green3","goldenrod4","black"), lty=c(1,1,1),lwd=2,cex=0.8)
 
 plot.2niche(kk_pca12,sn_pca12,pal50kk,pal50sn,"deeppink","green3")
+title("b) Savi's warbler compare to reed warbler [D=0.56]", adj = 0)
+legend("topright", legend=c("RW","SW", "Background"),
+       col=c("green3","deeppink","black"), lty=c(1,1,1),lwd=2,cex=0.8)
+
 plot.2niche(sn_pca12,grw_pca12,pal50sn,pal50grw,"goldenrod4","deeppink")
+title("c) Great reed warbler compare to Savi's warbler [D=0.46]", adj = 0)
+legend("topright", legend=c("GrW","SW", "Background"),
+       col=c("goldenrod4","deeppink","black"), lty=c(1,1,1),lwd=2,cex=0.8)
 
 #plot niche difference
 plot.2niche_diff <- function(kk_pca12,grw_pca12,pal50kk,pal50grw,col1,col2) {
   diff=grw_pca12$z.uncor-kk_pca12$z.uncor
   
   image(x=kk_pca12$x,y=kk_pca12$y,z=t(as.matrix(diff))[,nrow(as.matrix(diff)):1], col = alpha(pal50diff,1), zlim = c(-1,1), 
-        xlab = "PCA 1", ylab = "PCA 1")
+        xlab = "PCA 1", ylab = "PCA 2")
   
   grw_pca12$uncor.norm<-t(as.matrix(grw_pca12$z.uncor))[,nrow(as.matrix(grw_pca12$z.uncor)):1]
   contour(x=grw_pca12$x,y=grw_pca12$y,grw_pca12$uncor.norm, levels = quantile(grw_pca12$uncor.norm[grw_pca12$uncor.norm > 0], c(0, 0.5)), drawlabels = FALSE,
@@ -86,9 +93,19 @@ plot.2niche_diff <- function(kk_pca12,grw_pca12,pal50kk,pal50grw,col1,col2) {
 }
 
 plot.2niche_diff(kk_pca12,grw_pca12,pal50kk,pal50grw,"goldenrod4","green3")
-title("b) niche difference", adj = 0)
+#title("b) niche difference", adj = 0)
 legend("topright", legend=c("RW 100%","RW 50%","GrW 100%", "GrW 50%","Background 100%"),
        col=c("green3","green3","goldenrod4","goldenrod4","black"), lty=c(1,2,1,2,1),lwd=2,cex=0.8)
+
+plot.2niche_diff(kk_pca12,sn_pca12,pal50kk,pal50sn,"deeppink","green3")
+#title("b) niche difference", adj = 0)
+legend("topright", legend=c("RW 100%","RW 50%","SW 100%", "SW 50%","Background 100%"),
+       col=c("green3","green3","deeppink","deeppink","black"), lty=c(1,2,1,2,1),lwd=2,cex=0.8)
+
+plot.2niche_diff(sn_pca12,grw_pca12,pal50sn,pal50grw,"deeppink","goldenrod4")
+#title("b) niche difference", adj = 0)
+legend("topright", legend=c("GrW 100%","GrW 50%","SW 100%", "SW 50%","Background 100%"),
+       col=c("goldenrod4","goldenrod4","deeppink","deeppink","black"), lty=c(1,2,1,2,1),lwd=2,cex=0.8)
 
 # plot color bar
 color.bar <- function(lut, min, max, nticks=11, ticks=seq(min, max, len=nticks), title='') {
@@ -128,7 +145,7 @@ ecospat.plot.overlap.test2 <- function(x, type, title,pos) {
   h0 <- hist(sim, plot = FALSE, nclass = 10)
   y0 <- max(h0$counts)
   d=density(sim)
-  plot(d,xlim = xlim0, col = "blue", main = title, xlab = "D",sub = paste("p.value = ", round(p, 4)))
+  plot(d,xlim = xlim0, col = "blue", main = title, xlab = "D")
   legend(pos, legend=c(paste("mean sim.D=",round(mean(sim),2)),paste("sd sim.D=",round(sd(sim),2)),
                              paste("obs.D=",round(obs,2))),paste("p.value = ", round(p, 5)),cex=0.8)
   polygon(d, col="lightblue", border="blue")
@@ -137,7 +154,9 @@ ecospat.plot.overlap.test2 <- function(x, type, title,pos) {
   invisible()
 }
 
-ecospat.plot.overlap.test2(eq_grw_kk,"D","Equivalency","topleft")
+ecospat.plot.overlap.test2(eq_grw_kk,"D","Equivalency **","topleft")
+ecospat.plot.overlap.test2(eq_k_s,"D","Equivalency **","topleft")
+ecospat.plot.overlap.test2(eq_grw_s,"D","Equivalency **","topleft")
 
 ecospat.plot.overlap.test2(sim.test_gr_k,"D","Similiraty RW->GrW","topright")
 ecospat.plot.overlap.test2(sim.test_k_gr,"D","Similiraty GrW->RW","topright")
