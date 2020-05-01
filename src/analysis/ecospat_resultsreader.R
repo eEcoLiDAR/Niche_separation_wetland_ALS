3c9de72abbab9a0899403a146552f662ced29370
komazsofi <komazsofi@gmail.com>
2020-05-01 13:33:53 +0200

    small adapt

--- src/analysis/ecospat_resultsreader.R
+++ src/analysis/ecospat_resultsreader.R
@@ -1,7 +1,7 @@
 library(ecospat)
 
 # Global
-workingdirectory="C:/Koma/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v12/"
+workingdirectory="C:/Koma/Sync/_Amsterdam/_PhD/Chapter3_wetlandniche/3_Dataprocessing/Niche_v13/"
 setwd(workingdirectory)
 
 # PCA 1 vs PCA 2
